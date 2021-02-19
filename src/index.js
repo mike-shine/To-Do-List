28467c5fd28b5a890b14707ac7fb5fa6e22f2458
Mike Shine <michaelbshine@outlook.com>
2021-02-19 12:51:45 -0800

    modal windows close on close button click or click outside modal content

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
 const toDoModal = document.getElementById('toDoModal');
 const projectModal = document.getElementById('projectModal');
+const closeButton = document.getElementsByClassName('closeButton')[0];
 
 newToDoButton.addEventListener('click', () => {
   toDoModal.style.display = 'block';
@@ -17,6 +18,18 @@ newToDoButton.addEventListener('click', () => {
 newProjectButton.addEventListener('click', () => {
   projectModal.style.display= 'block';
 });
+
+closeButton.addEventListener('click', () => {
+  toDoModal.style.display = 'none';
+  // projectModal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === toDoModal || e.target === projectModal) {
+    toDoModal.style.display = 'none';
+    // projectModal.style.display = 'none';
+  }
+})
 
 
 
