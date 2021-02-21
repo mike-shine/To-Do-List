a2be88fb88259db9b927149e8105e2400f1ee889
Mike Shine <michaelbshine@outlook.com>
2021-02-20 18:22:20 -0800

    created selector menu on landing page

--- src/landingPage.js
+++ src/landingPage.js
@@ -8,8 +8,6 @@ const subHeader = document.createElement('h2');
 subHeader.textContent = 'Create a new project, or create a new to-do:';
 document.body.appendChild(subHeader);
 
-// create a html select tag menu here for users to select a project category to view
-// by default, on page load only unsorted to-dos are displayed
 
 const newProjectButton = document.createElement('button');
 newProjectButton.classList.add('newProjectButton');
@@ -20,6 +18,27 @@ const newToDoButton = document.createElement('button');
 newToDoButton.classList.add('newToDoButton');
 newToDoButton.textContent = 'New to-do';
 document.body.appendChild(newToDoButton);
+
+const lineBreak = document.createElement('br');
+document.body.appendChild(lineBreak);
+
+const projectLabel = document.createElement('label');
+projectLabel.textContent = 'Select a project to view: ';
+const projectSelector = document.createElement('select');
+projectLabel.appendChild(projectSelector);
+const unsortedOption = document.createElement('option');
+unsortedOption.textContent = '';
+projectSelector.appendChild(unsortedOption);
+
+const aroundTheHouseOption = document.createElement('option');
+aroundTheHouseOption.textContent = 'Around the House';
+projectSelector.appendChild(aroundTheHouseOption);
+const backyardShedOption = document.createElement('option');
+backyardShedOption.textContent = 'Backyard Shed';
+projectSelector.appendChild(backyardShedOption);
+
+document.body.appendChild(projectLabel);
+
 
 const mowLawn = makeToDo('Around the House', 'mow the lawn', 'cut the blades of grass shorter', 'next thursday', 'low');
 
